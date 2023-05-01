dc88de8bd660eb7c205cd460ab13b017c854bdc2
WendelHime <wendelhime@hotmail.com>
2023-05-01 07:10:53 -0300

    feat: add more tests to the port service

--- internal/logic/port_service_test.go
+++ internal/logic/port_service_test.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"errors"
 	"io"
 	"strings"
 	"testing"
@@ -21,6 +22,67 @@ func TestSyncPorts(t *testing.T) {
 		setup  func(t *testing.T) (io.Reader, PortDomainService)
 	}{
 		{
+			name: "empty port input should return a bad request",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrBadRequest)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+
+				return strings.NewReader(""), NewPortDomainService(portRepo)
+			},
+		},
+		{
+			name: "invalid input should return a bad request",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrBadRequest)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+
+				return strings.NewReader("test"), NewPortDomainService(portRepo)
+			},
+		},
+		{
+			name: "invalid unloc should return a internal server error",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrInternalServerError)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+
+				return strings.NewReader("{1: {}}"), NewPortDomainService(portRepo)
+			},
+		},
+		{
+			name: "invalid object to decode should return an internal server error",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrInternalServerError)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+
+				return strings.NewReader(`{"1": ""`), NewPortDomainService(portRepo)
+			},
+		},
+		{
+			name: "unexpected error when retrieving port should return an internal server error",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrInternalServerError)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+				portRepo.EXPECT().Get(gomock.Any(), gomock.Any()).Return(models.Port{}, errors.New("random error")).MaxTimes(1)
+
+				return strings.NewReader(threeRandomPorts()), NewPortDomainService(portRepo)
+			},
+		},
+		{
 			name: "add new ports with success",
 			assert: func(t *testing.T, err error) {
 				assert.Nil(t, err)
@@ -30,6 +92,34 @@ func TestSyncPorts(t *testing.T) {
 				portRepo := storage.NewMockPortRepository(ctrl)
 				portRepo.EXPECT().Get(gomock.Any(), gomock.Any()).Return(models.Port{}, localErrs.ErrNotFound).MaxTimes(3)
 				portRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil).MaxTimes(3)
+
+				return strings.NewReader(threeRandomPorts()), NewPortDomainService(portRepo)
+			},
+		},
+		{
+			name: "failure to add new port should return an internal server error",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrInternalServerError)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+				portRepo.EXPECT().Get(gomock.Any(), gomock.Any()).Return(models.Port{}, localErrs.ErrNotFound).MaxTimes(3)
+				portRepo.EXPECT().Create(gomock.Any(), gomock.Any()).Return(errors.New("random error")).MaxTimes(1)
+
+				return strings.NewReader(threeRandomPorts()), NewPortDomainService(portRepo)
+			},
+		},
+		{
+			name: "failure to update port should return an internal server error",
+			assert: func(t *testing.T, err error) {
+				assert.ErrorIs(t, err, localErrs.ErrInternalServerError)
+			},
+			setup: func(t *testing.T) (io.Reader, PortDomainService) {
+				ctrl := gomock.NewController(t)
+				portRepo := storage.NewMockPortRepository(ctrl)
+				portRepo.EXPECT().Get(gomock.Any(), gomock.Any()).Return(models.Port{}, nil).MaxTimes(3)
+				portRepo.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.New("random error")).MaxTimes(1)
 
 				return strings.NewReader(threeRandomPorts()), NewPortDomainService(portRepo)
 			},
